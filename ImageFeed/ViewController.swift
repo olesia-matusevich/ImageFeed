7f2b7c4e58f3d61b2583f7f290792ae857aac421
olesia-matusevich <olesia.matusevich@gmail.com>
2024-11-25 13:23:34 +0100

    no message

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Alesia Matusevich on 21/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
