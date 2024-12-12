3780a6d5d8b3af55db0493aef2b8720402abe785
olesia-matusevich <olesia.matusevich@gmail.com>
2024-12-12 12:49:52 +0100

    Исправлены замечания после ревью: переменные настраиваются при объявлении, сокращен дублирующийся код, констрейнты активируются с помощью NSLayoutConstraints.activate

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -11,108 +11,95 @@ final class ProfileViewController: UIViewController {
     
     // MARK: - Private Properties
     
-    private lazy var profileImageView = UIImageView()
-    private lazy var userNameLabel = UILabel()
-    private lazy var userLoginLabel = UILabel()
-    private lazy var descriptionLabel = UILabel()
-    private lazy var logoutButton = UIButton(type: .custom)
+    private let profileImageView: UIImageView = {
+        let view = UIImageView()
+        let image = UIImage(named: "UserImage")
+        view.image = image
+        return view
+    }()
+    
+    private let userNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Екатерина Новикова"
+        label.textColor = .white
+        label.font = .boldSystemFont(ofSize: 23)
+        return label
+    }()
+    
+    private let userLoginLabel: UILabel = {
+        let label = UILabel()
+        label.text = "@ekaterina_nov"
+        label.textColor = UIColor(named: "CustomGreyColorLogin")
+        label.font = .systemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello, World!"
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 13)
+        return label
+    }()
+    
+    private let logoutButton: UIButton = {
+        let button = UIButton(type: .custom)
+        let logoutImage =  UIImage(named: "LogoutImage")
+        button.setImage(logoutImage, for: button.state)
+        button.addTarget(
+            nil,
+            action: #selector(Self.didTapButton),
+            for: .touchUpInside
+        )
+        return button
+    }()
     
     // MARK: - Overrides Methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        configureProfileImageView()
-        configureUserNameLabel()
-        configureUserLoginLabel()
-        configureDescriptionLabel()
-        configureLogoutButton()
-   
-        createСonstraints()
+        setupViews()
+        setupСonstraints()
     }
     
     // MARK: - Private Methods
     
     @objc
-       private func didTapButton() {
-//           TODO: Добавить обработчик нажатия кнопки логаута
-       }
-    
-    private func configureProfileImageView() {
-        let profileImage = UIImage(named: "UserImage")
-        profileImageView.image = profileImage
-        
-        profileImageView.translatesAutoresizingMaskIntoConstraints = false
-       
-        view.addSubview(profileImageView)
+    private func didTapButton() {
+        // TODO: Добавить обработчик нажатия кнопки логаута
     }
     
-    private func configureUserNameLabel() {
-        userNameLabel.text = "Екатерина Новикова"
-        userNameLabel.textColor = .white
-        userNameLabel.font = .boldSystemFont(ofSize: 23)
-        
-        userNameLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(userNameLabel)
+    private func setupViews() {
+        [profileImageView, userNameLabel, userLoginLabel, descriptionLabel, logoutButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
     }
     
-    private func configureUserLoginLabel(){
-        userLoginLabel.text = "@ekaterina_nov"
-        userLoginLabel.textColor = UIColor(named: "CustomGreyColorLogin")
-        userLoginLabel.font = .systemFont(ofSize: 13)
-        
-        userLoginLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(userLoginLabel)
-    }
-    
-    private func configureDescriptionLabel(){
-        descriptionLabel.text = "Hello, World!"
-        descriptionLabel.textColor = .white
-        descriptionLabel.font = .systemFont(ofSize: 13)
-        
-        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(descriptionLabel)
-    }
-    
-    private func configureLogoutButton(){
-        let logoutImage =  UIImage(named: "LogoutImage")
-        logoutButton.setImage(logoutImage, for: logoutButton.state)
-        logoutButton.addTarget(
-            self,
-            action: #selector(Self.didTapButton),
-            for: .touchUpInside
-        )
-        
-        logoutButton.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(logoutButton)
-    }
-    
-    private func createСonstraints(){
-        // фото пользователя
-        profileImageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
-        profileImageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
-        profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
-        profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20).isActive = true
-        
-        // имя пользователя
-        userNameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
-        userNameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8).isActive = true
-       
-        // логин
-        userLoginLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
-        userLoginLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 8).isActive = true
-        
-        // дискрипшен
-        descriptionLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
-        descriptionLabel.topAnchor.constraint(equalTo: userLoginLabel.bottomAnchor, constant: 8).isActive = true
-       
-        // кнопка логаута
-        logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
-        logoutButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor).isActive = true
+    private func setupСonstraints(){
+        NSLayoutConstraint.activate([
+            // фото профиля
+            profileImageView.heightAnchor.constraint(equalToConstant: 70),
+            profileImageView.widthAnchor.constraint(equalToConstant: 70),
+            profileImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            profileImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            
+            // имя
+            userNameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            userNameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 8),
+            
+            // логин
+            userLoginLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            userLoginLabel.topAnchor.constraint(equalTo: userNameLabel.bottomAnchor, constant: 8),
+            
+            // дискрипшен
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            descriptionLabel.topAnchor.constraint(equalTo: userLoginLabel.bottomAnchor, constant: 8),
+            
+            // кнопка логаута
+            logoutButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            logoutButton.centerYAnchor.constraint(equalTo: profileImageView.centerYAnchor)
+        ])
     }
 }
 
